87ae8392ace44769332c98a534e559c465fa57f5
ZipCPU <dgisselq@ieee.org>
2024-01-30 22:54:11 -0500

    WIP: AutoFPGA runs, rtl doesnt yet build

---
+++ rtl/builddate.v
@@ -0,0 +1,48 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Filename: 	builddate.v
+// {{{
+// Project:	Demonstration Mercury KX2 project
+//
+// Purpose:	This file records the date of the last build.  Running "make"
+//		in the main directory will create this file.  The `define found
+//	within it then creates a version stamp that can be used to tell which
+//	configuration is within an FPGA and so forth.
+//
+// Creator:	Dan Gisselquist, Ph.D.
+//		Gisselquist Technology, LLC
+//
+////////////////////////////////////////////////////////////////////////////////
+// }}}
+// Copyright (C) 2021-2024, Gisselquist Technology, LLC
+// {{{
+// This file is part of the KIMOS project.
+//
+// The KIMOS project is free software and gateware: you can redistribute it
+// and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or (at
+// your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program.  (It's in the 1000 4 24 27 30 46 122 133 134 1000ROOT)/doc directory, run make with no
+// target there if the PDF file isn't present.)  If not, see
+// <http://www.gnu.org/licenses/> for a copy.
+// }}}
+// License:	GPL, v3, as defined and found on www.gnu.org,
+// {{{
+//		http://www.gnu.org/licenses/gpl.html
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+// }}}
+`ifndef	DATESTAMP
+`define DATESTAMP 32'h20240130
+`define BUILDTIME 32'h00225056
+`endif
+//
